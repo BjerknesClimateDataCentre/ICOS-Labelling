e25196390003e8117137bfd6a38001ca818121dc
Camilla <camilla.landa@uib.no>
2021-08-12 21:06:07 +0200

    (#6b) QC Summary and Plot: QC summary has parameter specific output

--- QC_summary_and_plot/1.QC_summary.R
+++ QC_summary_and_plot/1.QC_summary.R
@@ -18,10 +18,12 @@
 # INPUT PARAMETERS
 #-------------------------------------------------------------------------------
 
-# Specify the name of the parameters to create summary for:
+# What type of station (SOOP or FOS)
+station_type <- "SOOP"
+
+# Original column name of the measured CO2
 raw_CO2_colname <- "S1_CO2w"
 
-#summary_params <- c("xco2_wet_cal")
 
 #-------------------------------------------------------------------------------
 # INITIAL SETTINGS
@@ -65,119 +67,80 @@ colnames(df)[which(names(df) == paste(raw_CO2_colname," QC Comment",sep=""))] <-
 
 
 #-------------------------------------------------------------------------------
-# CREATE SUMMARY
+# FUNCTION(S)
 #-------------------------------------------------------------------------------
 
-#summaryfile_path <- paste("output/QC_summary_", datafile_name, ".txt", sep="")
-#sink(summaryfile_path)
-#cat("QC SUMMARY FOR datafile_name \n\n\n", sep="")
+# This function takes a column/parameter name and prints its QC summary
+QC_summary <- function(param_name){
+  
+  # Print the parameter name as subheader
+  cat("\n------------------------------------------------------------\n")
+  cat(param_name,":\n", sep="")
+  cat("----------\n")
+  
+  # Filter out rows with nan for the current paramter
+  df_filter <- df %>% 
+    filter(!is.na(as.numeric(!!as.symbol(param_name))))
+  
+  cat("  Total rows: ", nrow(df_filter),"\n\n", sep="")
+  
+  # Get and print flag frequency
+  flag_counts <- df_filter %>%
+    group_by(flags = !!as.symbol(paste(param_name, "_flag", sep=""))) %>%
+    summarize(n=n())
+  
+  for (flag in flag_counts$flags) {
+    count <- flag_counts$n[which(flag_counts$flags==flag)]
+    percent <- round((count/nrow(df_filter))*100,2)
+    cat("  Rows with flag ", flag,": ", count, " (", percent, "%)\n", sep="")
+  } 
+  
+  # Get and print the QC message frequency
+  untidy_message_counts <- df_filter %>%
+    group_by(messages = !!as.symbol(paste(param_name, "_comm", sep=""))) %>%
+    summarize(n=n()) %>%
+    filter(!is.na(messages))
+  
+  if (nrow(untidy_message_counts) != 0) {
+    message_counts <- untidy_message_counts %>%
+      mutate(messages = strsplit(messages,";")) %>%
+      unnest(messages) %>%
+      filter(messages != "") %>%
+      group_by(messages) %>%
+      summarize(n=sum(n)) %>%
+      arrange(desc(n))
+    
+    cat("\n  QC Messages\n")
+    for (message in message_counts$messages) {
+      count <- message_counts$n[which(message_counts$messages==message)]
+      percent <- round((count/nrow(df_filter))*100,2)
+      cat("    '", message,"': ", count, " (", percent, "%)\n", sep="")
+    }
+  }
+}
 
 
-#-------------------------------
-# FOR EACH PARAMETER:
+#-------------------------------------------------------------------------------
+# CREATE THE SUMMARY FILE
+#-------------------------------------------------------------------------------
 
+summaryfile_path <- paste("output/QC_summary_", datafile_name, ".txt", sep="")
+sink(summaryfile_path)
+cat("=================================================================\n")
+cat("QC SUMMARY FOR '", datafile_name, "'\n", sep="")
+cat("===========\n\n")
 
-# Filter out rows with nan for the current paramter
-df_filter <- df %>% 
-  filter(!is.na(as.numeric(raw_CO2)))
+# Create list of parameters to do summaries for
+if (station_type == "SOOP"){
+  param_list <- c("lat","lon","temp","teq","peq")
+} else {
+  param_list <- c("lat","lon","temp","sal")
+}
+param_list <- append(param_list, c("raw_CO2","fco2"))
 
-# Store the total number of measurements
-n_meas <- nrow(df_filter)
+# Create and print the QC summaries
+for (param in param_list){
+  QC_summary(param)
+} 
 
-# Store the flag frequency
-flag_counts <- df_filter %>%
-  group_by(raw_CO2_flag) %>%
-  summarize(n=n())
-
-# Store the QC message frequency
-untidy_message_counts <- df_filter %>%
-  group_by(raw_CO2_comm) %>%
-  summarize(n=n()) %>%
-  filter(!is.na(raw_CO2_comm))
-
-message_counts <- untidy_message_counts %>%
-  mutate(raw_CO2_comm = strsplit(raw_CO2_comm,";")) %>%
-  unnest(raw_CO2_comm) %>%
-  filter(raw_CO2_comm != "") %>%
-  group_by(raw_CO2_comm) %>%
-  summarize(count=sum(n))
-
-
-
-
-#for (i in 1:length(message_names)) {
-#	cat(message_names[i], ": ", message_counts[i], " (", format(round(message_counts[i] / nrow(data) * 100, 2), nsmall=2), "%)\n", sep="")
-#}
-
-#sink()
-
-
-
-# Exrtract values from counts tibbles:
-# flag_counts$n[which(flag_counts$xco2_wet_cal_flag==2)]
-
-
-
-#---------------------------
-# Use fco2 columns in the end to kind of sum up the results (since these are
-# accumulated)
-
-#cat("Total rows: ", total_row, "\n", sep="")
-#cat("Rows with messages: ", message_rows," (" ,perc,"%)","\n\n", sep="")                  
-#cat("QC Messages\n")
-#cat("===========\n")
-
-
-
-
-
-##########################
-### OLD
-#cat("\r", input_files, "               ")
-
-# Get the message counts
-#message_names <- vector(mode="character", length=0)
-#message_counts <- vector(mode="numeric", length=0)
-#message_rows <- 0
-
-
-#for (row in 1:nrow(df)) {
-#  messages <- as.character(data[["fCO2..uatm..QC.Comment"]][row])
-
-#  if (nchar(messages) > 0) {
-#    message_rows <- message_rows + 1
-#    message_list <- unlist(strsplit(messages, ";"))
-
-#    for (m in 1:length(message_list)) {
-#      message <- as.character(message_list[m])
-
-#      if (nchar(message) > 0) {
-#        message_index <- which(message_names == message)
-
-#          if (length(message_index) == 0) {
-#            message_names[length(message_names) + 1] <- message
-#            message_counts[length(message_counts) + 1] <- 1
-#            } else {
-#              message_counts <- replace(message_counts, message_index, message_counts[message_index] + 1)
-#            }
-#      }
-#    }
-#  }
-#}
-
-#total_row <- length(data[,1]) 
-#perc <- round(((message_rows/total_row)*100),2)
-
-#summaryfile_path <- paste("output/QC_summary_", datafile_name, ".txt", sep="")
-#sink(summaryfile_path)
-#cat("SUMMARY FOR FILE ", datafile_name, "\n\n\n", sep="")
-#cat("Total rows: ", total_row, "\n", sep="")
-#cat("Rows with messages: ", message_rows," (" ,perc,"%)","\n\n", sep="")                  
-#cat("QC Messages\n")
-#cat("===========\n")
-
-#for (i in 1:length(message_names)) {
-#	cat(message_names[i], ": ", message_counts[i], " (", format(round(message_counts[i] / nrow(data) * 100, 2), nsmall=2), "%)\n", sep="")
-#}
-
-#sink()
+sink()
