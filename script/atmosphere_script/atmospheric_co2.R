e489e9ae1363c66fd2cf048b3ed23c3d443741cd
Camilla <camilla.landa@uib.no>
2021-09-17 12:25:14 +0200

    (#22) Atmos: Create 'map' showing standard deviation

--- script/atmosphere_script/atmospheric_co2.R
+++ script/atmosphere_script/atmospheric_co2.R
@@ -71,7 +71,7 @@ create_letter_position <- function(letter_position_name, y_name, x_name,
   if (positions_template$ypos[position_index] > 0) {
     
     if (is.na(y_lims[2])){
-      ypos <- max(na.omit(as.numeric(df_mod[[y_name]])))
+      ypos <- max(na.omit(as.numeric(df_mod_full[[y_name]])))
     } else {
       ypos <- na.omit(y_lims[2])
     }
@@ -79,7 +79,7 @@ create_letter_position <- function(letter_position_name, y_name, x_name,
   } else {
     
     if (is.na(y_lims[1])) {
-      ypos <- min(na.omit(as.numeric(df_mod[[y_name]])))
+      ypos <- min(na.omit(as.numeric(df_mod_full[[y_name]])))
     } else {
       ypos <- na.omit(y_lims[1])
     }
@@ -89,7 +89,7 @@ create_letter_position <- function(letter_position_name, y_name, x_name,
   if (positions_template$xpos[position_index] > 0){
     
     if (is.na(x_lims[2])) {
-      xpos <- max(na.omit(as.numeric(df_mod[[x_name]])))
+      xpos <- max(na.omit(as.numeric(df_mod_full[[x_name]])))
     } else {
       xpos <- na.omit(x_lims[2])
     }
@@ -97,7 +97,7 @@ create_letter_position <- function(letter_position_name, y_name, x_name,
   } else {
     
     if (is.na(x_lims[1])) {
-      xpos <- min(na.omit(as.numeric(df_mod[[x_name]])))
+      xpos <- min(na.omit(as.numeric(df_mod_full[[x_name]])))
     } else {
       xpos <- na.omit(x_lims[1])
     }
@@ -123,7 +123,8 @@ create_letter_position <- function(letter_position_name, y_name, x_name,
 #-------------------------------------------------------------------------------
 # MODIFY THE DATA 
 #-------------------------------------------------------------------------------
-# Remove all unnecessary columns and rows and specify the data time
+# Specify date and time; remove all unnecessary columns and rows; get the 
+# sequence number and calculate mean and standard deviation per sequence
 
 # Extract the required column names from the settings
 for (column_key in names(settings$required_columns)) {
@@ -144,7 +145,25 @@ df_mod <- df %>%
          longitude = all_of(lon_colname),
          run_type = all_of(run_type_colname),
          co2 = all_of(co2_colname)) %>%
-  filter(run_type == settings$atm_type_name)
+  filter(run_type == settings$atm_type_name) %>%
+  select(-run_type)
+
+# Add sequence number and calculate mean and standard deviation per sequence
+df_mod_full <- df_mod %>%  
+  mutate(new_sequence = ifelse(
+    is.na(abs(difftime(df_mod$datetime, lag(df_mod$datetime), units="secs"))),
+    TRUE, ifelse(
+      abs(difftime(df_mod$datetime, lag(df_mod$datetime), units="secs")) > 3600,
+      TRUE, FALSE))) %>%
+  mutate(sequence = cumsum(new_sequence)) %>%
+  select(-new_sequence) %>%
+  group_by(sequence) %>%
+  mutate(mean = mean(co2), 
+         std = sd(co2),
+         std_plot_size = case_when((std < 0.5) ~ 0.1,
+                                   (std > 0.5 & std < 1) ~ 0.25,
+                                   (std > 1 & std < 2) ~ 0.75,
+                                   TRUE ~ 6))
 
 
 #-------------------------------------------------------------------------------
@@ -152,8 +171,8 @@ df_mod <- df %>%
 #-------------------------------------------------------------------------------
 
 # Extract the required plot settings
-for (plot_key in names(settings$plot_settings)) {
-  assign(plot_key, settings$plot_settings[[plot_key]])
+for (plot_key in names(settings$plot_settings$co2_vs_time)) {
+  assign(plot_key, settings$plot_settings$co2_vs_time[[plot_key]])
 }
 
 # Make the plot ranges numeric
@@ -170,7 +189,7 @@ filename <- paste("output/1.atmpspheric_co2_vs_time.png", sep = "")
 png(filename)
 
 # Create the plot
-plot <-  ggplot(df_mod, aes(x = datetime, y = co2)) +
+plot_1 <-  ggplot(df_mod_full, aes(x = datetime, y = co2)) +
   geom_point() +
   xlab("Time") + ylab(y_lab) + 
   # Specify monthly ticks with short month names as label
@@ -191,11 +210,11 @@ plot <-  ggplot(df_mod, aes(x = datetime, y = co2)) +
 
 # Change the range in plot if specified in settings
 if (!is.na(y_lim_min)){
-  plot <- plot + ylim(y_lim_min, y_lim_max)
+  plot_1 <- plot_1 + ylim(y_lim_min, y_lim_max)
 }
 
 # Create the image
-print(plot)
+print(plot_1)
 dev.off()
 
 
@@ -206,12 +225,77 @@ dev.off()
 # Set up the file for stats
 #sink(file = "output/std_stats.txt")"
 
+# Print number of measureents outside the plotting area
+n_outside_plot <- sum(df_mod_full$co2 > y_lim_max) + sum(df_mod_full$co2 < y_lim_min)
+percent_outside <- round((n_outside_plot/nrow(df_mod_full))*100, 1)
+cat("Plot 1. Measurements outside plot area (", y_lim_min, ":", y_lim_max,
+    ") is: ", n_outside_plot, " (", percent_outside, "%)\n", sep = "")
+
+# Print number of bad measurements 
+good_min <- as.numeric(settings$qc_range$good_min)
+good_max <- as.numeric(settings$qc_range$good_max)
+n_bad <- sum(df_mod_full$co2 < good_min) + sum(df_mod_full$co2 > good_max)
+percent_bad <- round((n_bad/nrow(df_mod_full))*100, 1)
+cat("Plot 1. Number of bad measurements (outside the good range ", good_min,
+    ":", good_max, ") is: ", n_bad, " (", percent_bad, "%)\n", sep = "")
 
 
+#-------------------------------------------------------------------------------
+# CREATE STANDARD DEVIATION 'MAP'
+#-------------------------------------------------------------------------------
+
+# Extract the required plot settings
+for (plot_key in names(settings$plot_settings$std_dev_map)) {
+  assign(plot_key, settings$plot_settings$std_dev_map[[plot_key]])
+}
+
+# Make the plot ranges numeric
+y_lim_min <- as.numeric(y_lim_min)
+y_lim_max <- as.numeric(y_lim_max)
+x_lim_min <- as.numeric(x_lim_min)
+x_lim_max <- as.numeric(x_lim_max)
+
+# Get the letter position details
+letter_position <- create_letter_position(letter_position_name, 
+                        y_name = "latitude", x_name = "longitude", 
+                        y_lims = c(y_lim_min, y_lim_max),
+                        x_lims = c(x_lim_min,y_lim_max))
+
+# Set up the image file
+filename <- paste("output/2.std_dev_map.png", sep = "")
+png(filename)
+
+plot_2 <-  ggplot(df_mod_full, aes(x = longitude, y = latitude)) +
+  # Use the std_plot_size column to scale the points
+  geom_point(aes(size = std_plot_size), shape = 1, show.legend = F) +
+  # Icrease the size of the points in plot
+  scale_size(range = c(0,20)) +
+  xlab("Longitude") + ylab("Latitude") + 
+  # Change plot layout to another theme and so some adjustments to the theme
+  theme_bw() +
+  theme(text = element_text(family = "Times"),
+        axis.text = element_text(size = rel(1.5)),
+        axis.title = element_text(size = rel(1.7))) +
+  # Add the plot letter string
+  annotate("text",
+           x = letter_position[[1]],
+           y = letter_position[[2]], 
+           label = letter_string,
+           hjust = letter_position[[3]],
+           vjust = letter_position[[4]],
+           size = 9)
+
+# Change the range in plot if specified in settings
+if (!is.na(y_lim_min)){
+  plot_2 <- plot_2 + ylim(y_lim_min, y_lim_max)
+}
+if (!is.na(x_lim_min)){
+  plot_2 <- plot_2 + xlim(x_lim_min, x_lim_max)
+}
+
+# Create the image
+print(plot_2)
+dev.off()
 
 
 #sink()
-
-
-
-#Create a sequence column in the modified data
