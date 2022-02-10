f1b657dd798dd1d23edc18f9c0d10a8f46f183d3
Camilla <camilla.landa@uib.no>
2022-02-10 13:12:44 +0100

    (#38) Add single plot script + settings edits

---
+++ script/single_plot/single_plot.R
@@ -0,0 +1,192 @@
+################################################################################
+### MAKE SINGLE PLOT (ALWAYS VS TIME)                                        ###
+################################################################################
+
+#------------------------------------------------------------------------------
+# INPUT PARAMETERS
+
+input_filename <- "input/GO_all.txt"
+letter_corner <- "topleft"
+y_colname <- "H2O_flow"
+date_colname <- "PC_Date"
+time_colname <- "PC_Time"
+datetime_format <- "%d/%m/%y %H:%M:%S"
+y_label <- "Water Flow (L/min)"
+plot_letter <- "h)"
+output_filename <- "output/waterflow_vs_time.png"
+y_lims <- c(NA,NA)
+x_lims <- c(NA,NA)
+
+
+#-------------------------------------------------------------------------------
+# INITIAL SETTINGS
+
+# Load packages
+library(readr)
+library(dplyr)
+library(jsonlite)
+library(ggplot2)
+
+# Set the locale (needed for correct spelling of months in plots)
+Sys.setlocale("LC_ALL", "English");
+
+# Change the plot font (subscript 2 does not work in the png with default font)
+windowsFonts(Times = windowsFont("Times New Roman"))
+
+# Remove existing files in the output directory
+if (!is.null(list.files("output"))) {
+  file.remove(dir(paste0(getwd(), "/output"), pattern = "", full.names = TRUE))
+}
+
+#-------------------------------------------------------------------------------
+# FUNCTIONS
+
+# Function that adds a date time column to the data. Required inputs are the
+# name of the data, its date and time colnames, and their formats.
+assign_datetime <- function(df, date_colname, time_colname, datetime_format){
+  df_datetime <- df %>%
+    mutate(datetime = case_when(
+      # ... the date and time is given in one column:
+      (date_colname == time_colname) ~
+        as.POSIXct(df[[date_colname]], format = datetime_format, tz = "UTC"),
+      # ... the date and time is in two columns:
+      (length(strsplit(date_colname, ",")) == 1 & 
+         length(strsplit(time_colname, ",")) == 1) ~
+        as.POSIXct(paste(df[[date_colname]], df[[time_colname]]),
+                   format = datetime_format, tz = "UTC"),
+      # ... the year, month, day, hour, minute and seconds are in separate cols:
+      # (have not tested if this bit works)
+      (length(strsplit(date_colname, ",")) != 1) ~
+        as.POSIXct(paste(
+          strsplit(date_colname,',')[1], strsplit(date_colname,',')[2],
+          strsplit(date_colname,',')[3], strsplit(time_colname,',')[1],
+          strsplit(time_colname,',')[2], strsplit(time_colname,',')[3]),
+          format = datetime_format, tz = "UTC"))
+    )
+  return(df_datetime)
+}
+
+# This function returns information about where in the plot to put the letter
+# string. Required inputs are the name of the parameter, and in which corner
+# of the plot the letter should be added.
+create_letter_position <- function(letter_position_name, y_name, x_name,
+                                   y_lims, x_lims) {
+  
+  # Get the row number in the positions template data frame where information 
+  # about the requested corner is given
+  position_index <- which(positions_template$location == letter_position_name)
+  
+  # The x and y-positions are either the max or min of the parameter. Which one
+  # is determined by the sign in the xpos and ypos column in the position 
+  # template. (!! This is confusing code - improve how this is done !!)
+  if (positions_template$ypos[position_index] > 0) {
+    
+    if (is.na(y_lims[2])){
+      ypos <- max(as.numeric(na.omit(df_to_plot[[y_name]])))
+    } else {
+      ypos <- na.omit(y_lims[2])
+    }
+    
+  } else {
+    
+    if (is.na(y_lims[1])) {
+      ypos <- min(as.numeric(na.omit(df_to_plot[[y_name]])))
+    } else {
+      ypos <- na.omit(y_lims[1])
+    }
+    
+  }
+  
+  if (positions_template$xpos[position_index] > 0){
+    
+    if (is.na(x_lims[2])) {
+      xpos <- max(na.omit(as.numeric(df_to_plot[[x_name]])))
+    } else {
+      xpos <- na.omit(x_lims[2])
+    }
+    
+  } else {
+    
+    if (is.na(x_lims[1])) {
+      xpos <- min(na.omit(as.numeric(df_to_plot[[x_name]])))
+    } else {
+      xpos <- na.omit(x_lims[1])
+    }
+    
+  }
+  
+  # Change xpos class back to posixct if x axis data is date time
+  if (x_name == "datetime") {
+    xpos <- as.POSIXct(xpos, origin = '1970-01-01')
+  }
+  
+  # The remaining letter position information is extracted straight from 
+  # the position template data frame
+  hjustvar <- positions_template$hjustvar[position_index]
+  vjustvar <- positions_template$vjustvar[position_index]
+  
+  # Store all position details in a vector and return it
+  letter_position <- list(xpos, ypos, hjustvar, vjustvar)
+  return(letter_position)
+}
+
+
+#-------------------------------------------------------------------------------
+# CREATE PLOT
+
+# Import data
+df <- read_tsv(input_filename)
+
+# Create datetime column
+df_to_plot <- assign_datetime(df, date_colname, time_colname, datetime_format)
+
+# Create a data frame with location information about each corner in a
+# plot. This data frame is later used to determine where to place the letter 
+# string in plots.
+positions_template <- data.frame(
+  xpos = c(-Inf, -Inf, Inf, Inf),
+  ypos = c(-Inf, Inf, -Inf, Inf),
+  hjustvar = c(-1, -1, 1, 1),
+  vjustvar = c(-1, 1, -1, 1),
+  location = c("bottomleft", "topleft", "bottomright", "topright"))
+
+# Create letter position
+letter_position <- create_letter_position(letter_corner, y_colname, "datetime",
+                                          y_lims, x_lims)
+
+# Set up the image file
+png(output_filename)
+
+# Create a ggplot and add multiple features
+ret <- ggplot(df_to_plot, 
+              aes(x = df_to_plot$datetime, y = df_to_plot[[y_colname]])) +
+  geom_point() +
+  xlab("Time") + ylab(y_label) + 
+  # Change plot layout to another theme and so some adjustments $o the theme
+  theme_bw() +
+  theme(text = element_text(family = "Times"),
+        axis.text = element_text(size = rel(1.5)),
+        axis.title = element_text(size = rel(1.7))) +
+  annotate("text",
+           x = letter_position[[1]],
+           y = letter_position[[2]], 
+           label = plot_letter,
+           hjust = letter_position[[3]],
+           vjust = letter_position[[4]],
+           size = 9)
+
+# Specify monthly ticks with short month names as label
+# The time between ticks depends on the length of the dataset
+timespan <- as.numeric(df_to_plot$datetime[nrow(df)]-df_to_plot$datetime[1])
+if (timespan < 240) {
+  breaks = "2 months"
+} else if (timespan > 240 & timespan < 750) {
+  breaks = "4 months"
+} else {
+  breaks = "6 months"
+}
+ret <- ret + scale_x_datetime(date_breaks = breaks, date_labels = '%b-%y')
+
+# Create the plot and image file
+print(ret)
+dev.off()
